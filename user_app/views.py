cbed11a5e1da8add435d111fae0b42beb1f1c68e
DONA PAUL <donapaul901@gmail.com>
2025-10-21 09:53:50 +0530

    fifth cmt

--- user_app/views.py
+++ user_app/views.py
@@ -4,10 +4,29 @@ from django.views.generic import View
 
 from user_app.forms import Userregisterform
 
+from user_app.models import User
+
 # Create your views here.
 class RegisterView(View):
     def get(self,request):
         form = Userregisterform()
         return render(request,"signup.html",{"form":form})
     
-    
+    def post(self,request):
+        print(request.POST)  
+        username =request.POST.get('username')
+
+        first_name =request.POST.get('first_name')
+
+        last_name =request.POST.get('last_name')
+
+        password =request.POST.get('password')  
+
+        email =request.POST.get('email')
+
+        User.objects.create_user(username=username,
+                                 first_name=first_name,
+                                 last_name=last_name,
+                                 password=password,
+                                 email=email)
+        return render(request,'signup.html')
