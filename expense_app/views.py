572e5d22b36395ed7a92ef4bd80d06f49f67ee78
DONA PAUL <donapaul901@gmail.com>
2025-10-24 10:31:29 +0530

    eighteen

--- expense_app/views.py
+++ expense_app/views.py
@@ -7,3 +7,13 @@ class Add_expense_view(View):
     def get(self,request):
         form = ExpenseForm()
         return render(request,"expense_add.html",{"form":form})
+    
+    def post(self,request):
+        print(request.POST)
+        form =ExpenseForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+        return render(request,"expense_add.html",{"form":form})    
