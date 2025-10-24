69a48e321f3fa10ffc1ce48bf8a6b06e8627a4c0
DONA PAUL <donapaul901@gmail.com>
2025-10-24 09:27:49 +0530

    sixteen

---
+++ expense_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from expense_app.models import Expense
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ["category","title","amount","payment_date","created_date"]
